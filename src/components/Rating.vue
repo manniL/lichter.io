e60322b5fc2cacde6bfedacc6d281dc63676f208
Alexander Lichter <manniL@gmx.net>
2017-11-11 20:23:42 +0100

    Add rating components

---
+++ src/components/Rating.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex">
+        <rating-square v-for="i in (1,5)" :key="i" :hidden="i > rating"></rating-square>
+    </div>
+</template>
+<script>
+  import RatingSquare from './RatingSquare.vue'
+
+  export default {
+    components: {RatingSquare},
+    props: {
+      rating: {
+        type: Number,
+        required: true,
+        validator: (value) => {
+          return value >= 0 && value <= 5
+        }
+      }
+    },
+    data () {
+      return {}
+    }
+  }
+</script>
