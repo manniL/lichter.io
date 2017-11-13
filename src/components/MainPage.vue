df0760724fea728f85e7ad9df5694bbbd52b3f36
Alexander Lichter <manniL@gmx.net>
2017-11-13 21:34:18 +0100

    Change HelloWorld to MainPage

---
+++ src/components/MainPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <navbar></navbar>
+        <intro></intro>
+        <about-me></about-me>
+        <skills></skills>
+        <timeline></timeline>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+  import Navbar from './Navbar.vue'
+  import Intro from './Intro.vue'
+  import AboutMe from './AboutMe.vue'
+  import Skills from './Skills.vue'
+  import Timeline from './Timeline.vue'
+  import AppFooter from './Footer.vue'
+
+  export default {
+    name: 'MainPage',
+    components: {
+      Navbar,
+      Intro,
+      AboutMe,
+      Skills,
+      Timeline,
+      AppFooter
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import MainPage from '@/components/MainPage.vue'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Lichter.io',
+      component: MainPage
     }
   ]
 })
