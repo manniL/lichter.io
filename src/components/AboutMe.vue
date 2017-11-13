71d6d8701125f4a3ac17ced45fcead4f63614a7e
Alexander Lichter <manniL@gmx.net>
2017-11-13 10:23:00 +0100

    Add animations to intro and about me
    + Remove obsolete divs

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,23 +1,21 @@
 <template>
     <section class="flex flex-wrap bg-grey-light py-8">
-        <div class="flex flex-col justify-around items-center w-5/6 md:w-1/2 mx-auto">
-            <div>
-                <p class="text-xl md:text-2xl text-black mb-2">Hey you!</p>
-                <p class="text-base md:text-lg text-grey-darker leading-normal">
-                    I'm <span class="text-grey-darkest">Alex</span>, a 20-year-old guy living in
-                    <span class="text-grey-darkest">Dresden, Germany</span>. Already at young age, I was impressed of
-                    computers and wanted to know more about them. Now, I'm a self-taught developer running his 
-                    <span class="text-grey-darkest">own company</span> and studying CS in the meantime.
-                    <br><br>
-                    When developing web apps, I rely on the <span class="text-grey-darkest">Laravel</span>
-                    framework for the backend most of time.
-                    As javascript frontend frameworks I choose <span class="text-grey-darkest">Vue.js</span>, while I 
-                    use <span class="text-grey-darkest">Bootstrap</span> or the newly released
-                    <span class="text-grey-darkest">Tailwind</span> in terms of CSS frameworks.
-                    <br><br>
-                    But of course I used a bunch of other technologies, depending on the projects aim and requirements!
-                </p>
-            </div>
+        <div class="justify-around items-center w-5/6 md:w-1/2 mx-auto animated fadeIn aboutMe">
+            <p class="text-xl md:text-2xl text-black mb-2">Hey you!</p>
+            <p class="text-base md:text-lg text-grey-darker leading-normal">
+                I'm <span class="text-grey-darkest">Alex</span>, a 20-year-old guy living in
+                <span class="text-grey-darkest">Dresden, Germany</span>. Already at young age, I was impressed of
+                computers and wanted to know more about them. Now, I'm a self-taught developer running his
+                <span class="text-grey-darkest">own company</span> and studying CS in the meantime.
+                <br><br>
+                When developing web apps, I rely on the <span class="text-grey-darkest">Laravel</span>
+                framework for the backend most of time.
+                As javascript frontend frameworks I choose <span class="text-grey-darkest">Vue.js</span>, while I
+                use <span class="text-grey-darkest">Bootstrap</span> or the newly released
+                <span class="text-grey-darkest">Tailwind</span> in terms of CSS frameworks.
+                <br><br>
+                But of course I used a bunch of other technologies, depending on the projects aim and requirements!
+            </p>
         </div>
     </section>
 </template>
