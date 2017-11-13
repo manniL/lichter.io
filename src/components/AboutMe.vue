c5d0e377d4214874c0eb5e0ee07e93bb8671c591
Alexander Lichter <manniL@gmx.net>
2017-11-13 11:25:01 +0100

    Polishing

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,7 +1,9 @@
 <template>
     <section class="flex flex-wrap bg-grey-light py-8">
         <div class="justify-around items-center w-5/6 md:w-1/2 mx-auto animated fadeIn aboutMe">
-            <p class="text-xl md:text-2xl text-black mb-2">Hey you!</p>
+            <p class="text-xl md:text-2xl text-black mb-2">
+                Hey you!
+            </p>
             <p class="text-base md:text-lg text-grey-darker leading-normal">
                 I'm <span class="text-grey-darkest">Alex</span>, a 20-year-old guy living in
                 <span class="text-grey-darkest">Dresden, Germany</span>. Already at young age, I was impressed of
--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -3,9 +3,12 @@
         <div class="flex flex-col justify-around items-center w-5/6 md:w-1/2 mx-auto text-center animated zoomIn">
             <img src="../assets/me.jpg" class="rounded-full w-64 border-4 border-grey-light shadow-lg mb-4">
             <div class="">
-                <h1 class="mb-2 font-light text-2xl md:text-3xl">Alexander Lichter</h1>
+                <h1 class="mb-2 font-light text-2xl md:text-3xl">
+                    Alexander Lichter
+                </h1>
                 <h2 class="font-hairline text-grey-dark text-base md:text-xl">
-                    Developer &bull; Founder &bull; Gamer</h2>
+                    Developer &bull; Founder &bull; Gamer
+                </h2>
             </div>
         </div>
     </section>
