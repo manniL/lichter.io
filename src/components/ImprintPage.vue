42ccff0ced9392459ed72c37e1ed3519d253ceba
Alexander Lichter <manniL@gmx.net>
2017-11-13 22:12:58 +0100

    Change imprint background

--- src/components/ImprintPage.vue
+++ src/components/ImprintPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <navbar></navbar>
-        <section class="flex flex-col flex-wrap items-center bg-grey-light py-8 leading-normal text-grey-darkest">
+        <section class="flex flex-col flex-wrap items-center bg-grey-lighter py-8 leading-normal text-grey-darkest">
             <div class="justify-around items-center w-5/6 md:w-1/2 mx-auto">
                 <h1>Legal Disclosure</h1>
                 Information in accordance with section 5 TMG
