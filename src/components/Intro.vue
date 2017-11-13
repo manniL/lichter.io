80e9460db14f0a964e3697ec1ddb59ae067840e4
Alexander Lichter <manniL@gmx.net>
2017-11-13 21:21:40 +0100

    Add contact buttons

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="flex flex-wrap bg-grey-lighter py-8">
+    <section class="flex flex-wrap bg-grey-lighter py-6">
         <div class="flex flex-col justify-around items-center w-5/6 md:w-1/2 mx-auto text-center animated zoomIn">
             <img src="../assets/me.jpg" class="rounded-full w-64 border-4 border-grey-light shadow-lg mb-4">
             <div class="">
@@ -9,6 +9,20 @@
                 <h2 class="font-hairline text-grey-dark text-base md:text-xl">
                     Developer &bull; Founder &bull; Gamer
                 </h2>
+            </div>
+            <div class="flex">
+                <a href="https://github.com/manniL" target="blank" rel="noreferrer noopener nofollow"
+                   class="text-center text-grey-dark no-underline bg-grey-light px-3 py-2 m-2 rounded fa fa-2x fa-github hover:text-black">
+                </a>
+                <a href="mailto:hello@lichter.io" target="blank" rel="noreferrer noopener nofollow"
+                   class="text-center text-grey-dark no-underline bg-grey-light px-3 py-2 m-2 rounded fa fa-2x fa-envelope hover:text-teal-darker">
+                </a>
+                <a href="tel:+4917670625208" target="blank" rel="noreferrer noopener nofollow"
+                   class="text-center text-grey-dark no-underline bg-grey-light px-3 py-2 m-2 rounded fa fa-2x fa-phone hover:text-green-dark">
+                </a>
+                <a href="https://twitter.com/TheAlexLichter" target="blank" rel="noreferrer noopener nofollow"
+                   class="text-center text-grey-dark no-underline bg-grey-light px-3 py-2 m-2 rounded fa fa-2x fa-twitter hover:text-blue">
+                </a>
             </div>
         </div>
     </section>
