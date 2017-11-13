fa64d99a413d50a805439f200b1887a870a93477
Alexander Lichter <manniL@gmx.net>
2017-11-13 20:17:43 +0100

    Fix translation

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -6,17 +6,17 @@
             </p>
             <p class="text-base md:text-lg text-grey-darker leading-normal">
                 I'm <span class="text-grey-darkest">Alex</span>, a 20-year-old guy living in
-                <span class="text-grey-darkest">Dresden, Germany</span>. Already at young age, I was impressed of
-                computers and wanted to know more about them. Now, I'm a self-taught developer running his
+                <span class="text-grey-darkest">Dresden, Germany</span>. Already at young age, Computers impressed me
+                and I wanted to know more about them. Now, I'm a self-taught developer running my
                 <span class="text-grey-darkest">own company</span> and studying CS in the meantime.
                 <br><br>
                 When developing web apps, I rely on the <span class="text-grey-darkest">Laravel</span>
-                framework for the backend most of time.
+                framework for the backend most of the time.
                 As javascript frontend frameworks I choose <span class="text-grey-darkest">Vue.js</span>, while I
                 use <span class="text-grey-darkest">Bootstrap</span> or the newly released
                 <span class="text-grey-darkest">Tailwind</span> in terms of CSS frameworks.
                 <br><br>
-                But of course I used a bunch of other technologies, depending on the projects aim and requirements!
+                But I also used a bunch of other technologies, each depending on the projects aim and requirements!
             </p>
         </div>
         <i class="fa fa-5x fa-caret-down mt-4 animated infinite bounceNice animation-duration-5 text-grey"></i>
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -8,37 +8,35 @@
                     Yeah, that's no joke! I played a Mario clone on my grandpa's PC.
                 </timeline-item>
                 <timeline-item when="2010" title="First basic website">
-                    I started looking into HTML and CSS because I wanted to create a website for a project
-                    I worked on with a friend. This worked out, though that website was quite crappy (as expected).
-                    It even used HTML frames 😅.<br>
+                    I started looking into HTML and CSS for a project
+                    I worked on with a friend. It went well, though the outcome wasn't as elegant as it'd be today (as expected).
+                    The website even made use of HTML frames 😅.<br>
                     But all beginnings are difficult.
                 </timeline-item>
                 <timeline-item when="Early 2011" title="First line of code">
                     After getting in touch with basic HTML and CSS a few months before, I started learning
                     Java. The motivation was quite simple: There was an insane hype around "Minecraft", and
-                    the plugins were written in Java. Because I wanted to learn programming in general, I
-                    thought starting with Java shouldn't be a bad decision.
-                    My goal was to learn Java in general, not only for Minecraft.
+                    the plugins were written in Java. Because I wanted to learn writing programs in general (not only to extend the game), I
+                    kind of thought starting with Java shouldn't be a bad decision.
                     <br><br>
                     <b>Fun fact</b>:
                     My first program wasn't Hello World, because I wanted to do something more unique instead
                 </timeline-item>
 
                 <timeline-item :period="true" title="After plunging into the world of code...">
-                    ... I wanted to learn more and more. While developing stuff from simple tools to GUI based
-                    applications with JSON processing, I also looked into databases (MySQL) and PHP.
-                    <br>
-                    The concept of dynamic websites fascinated me a lot.
+                    ... I wanted to learn even more. While developing stuff from simple tools right up to GUI based
+                    applications with JSON processing, I also looked into databases (MySQL in particular) and PHP, because
+                    the concept of dynamic websites fascinated me a lot.
                 </timeline-item>
 
                 <timeline-item when="November 11, 2011" title="First internship">
-                    In the 9th grade, we must fulfill a work experience at a company of our choice. Of course,
-                    we must apply for that internship regularly. My first thought was a computer repair company
+                    In the 9th grade, we had to fulfill a work experience at a company of our choice. Of course,
+                    we must apply for that internship regularly. I first thought of working at a computer repair company
                     to gain knowledge about the hardware, but then I applied at a smaller software company in
                     my town and they accepted my application!
                     <br><br>
                     My first task was testing their current system and trying to break it. After reporting the
-                    found bugs and caveats they saw that my understanding was quite good for a 15 year old guy,
+                    found bugs and caveats they noticed that my understanding was quite good for a 15-year-old guy,
                     so the follow-up tasks were more complex and deep, like creating configs and setting up
                     the database.
                     <br><br>
@@ -46,7 +44,7 @@
                     Redmine, MySQL, YAML, PHP (Symfony)
                 </timeline-item>
                 <timeline-item when="July 2015" title="Graduate school - Abitur (1,9)">
-                    In 2015 I finished school and did my Abitur (the German A-Level) at the
+                    In 2015 I finished school and did my Abitur (the German A-Levels) at the
                     <em>Europäisches Gymnasium Waldenburg</em> with a final grade of <strong>1.9</strong>
                 </timeline-item>
                 <timeline-item when="August 2015" title="Founded Developmint">
@@ -54,7 +52,7 @@
                     <a href="https://developmint.de" class="developmint no-underline" target="_blank">Developmint</a>
                     <small class="text-xs text-grey">(fancy, eh?)</small>
                     fulfills contract work for mid-sized businesses as well as single persons. Our core competencies are
-                    web apps, websites and MacOS/iOS apps. We did a range of work by now, ranging from simple but good-looking portfolio pages to complex
+                    web apps, websites and MacOS/iOS apps. We have done a range of work by now, ranging from simple but good-looking portfolio pages to complex
                     business tools.<br><br>
                     <strong>Interested?</strong> Drop me a line! 😉
                     <!-- TODO: Add link/cta here -->
