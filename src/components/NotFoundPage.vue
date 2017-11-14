a0bac475b41cb93d2785c48f5e68cfb0b977d46e
Alexander Lichter <manniL@gmx.net>
2017-11-14 09:48:34 +0100

    Change gif link to https

--- src/components/NotFoundPage.vue
+++ src/components/NotFoundPage.vue
@@ -5,7 +5,7 @@
             <h1 class="my-2">Nothing to see here!</h1>
             <p class="leading-loose mt-4">Looks like you took a wrong path!</p>
             <router-link to="/" class="text-grey-darkest my-4">Back</router-link>
-            <img src="http://thecatapi.com/api/images/get?format=src&type=gif">
+            <img src="https://thecatapi.com/api/images/get?format=src&type=gif">
         </section>
     </div>
 </template>
