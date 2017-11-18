d77b23fdef5331a08d45989082a77fa0c7965ece
Alexander Lichter <manniL@gmx.net>
2017-11-18 14:58:19 +0100

    Change image dimensions

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="flex flex-wrap bg-grey-lighter py-6">
         <div class="flex flex-col justify-around items-center w-5/6 md:w-1/2 mx-auto text-center animated zoomIn">
-            <img src="../assets/me.png" width="256" height="256"
+            <img src="../assets/me.png" width="255" height="255"
                  class="rounded-full w-64 border-4 border-grey-light shadow-lg mb-4">
             <div class="">
                 <h1 class="mb-2 font-light text-2xl md:text-3xl">
