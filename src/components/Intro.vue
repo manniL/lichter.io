12f03c42424d83204f453be5b4344ef0b5ad1421
Alexander Lichter <manniL@gmx.net>
2017-11-18 15:00:18 +0100

    Add specific border style

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,7 +2,7 @@
     <section class="flex flex-wrap bg-grey-lighter py-6">
         <div class="flex flex-col justify-around items-center w-5/6 md:w-1/2 mx-auto text-center animated zoomIn">
             <img src="../assets/me.png" width="255" height="255"
-                 class="rounded-full w-64 border-4 border-grey-light shadow-lg mb-4">
+                 class="rounded-full border-solid w-64 border-4 border-grey-light shadow-lg mb-4">
             <div class="">
                 <h1 class="mb-2 font-light text-2xl md:text-3xl">
                     Alexander Lichter
