01c73e56b1165270c39b1baf9855e5648f987782
Alexander Lichter <manniL@gmx.net>
2017-12-13 00:48:02 +0100

    Rewrite about me text

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -5,18 +5,20 @@
                 Hey you!
             </p>
             <p class="text-base md:text-lg text-grey-darker leading-normal">
-                I'm <span class="text-grey-darkest">Alex</span>, a 20-year-old guy living in
-                <span class="text-grey-darkest">Dresden, Germany</span>. Already at young age, Computers impressed me
-                and I wanted to know more about them. Now, I'm a self-taught developer running my
-                <span class="text-grey-darkest">own company</span> and studying CS in the meantime.
+                I'm <span class="text-grey-darkest">Alex</span>, a guy in his twenties living in
+                <span class="text-grey-darkest">Dresden, Germany</span>. Already at young age, computers impressed me
+                so much that I wanted to know more about them. Now, I'm a self-taught developer who runs his
+                <span class="text-grey-darkest">own company</span> and studies CS in the meantime.
                 <br><br>
-                When developing web apps, I rely on the <span class="text-grey-darkest">Laravel</span>
-                framework for the backend most of the time.
-                As javascript frontend frameworks I choose <span class="text-grey-darkest">Vue.js</span>, while I
-                use <span class="text-grey-darkest">Bootstrap</span> or the newly released
-                <span class="text-grey-darkest">Tailwind</span> in terms of CSS frameworks.
+                When developing web apps, I mostly rely on the <span class="text-grey-darkest">Laravel</span>
+                framework for the backend.
+                In terms of frontend, <span class="text-grey-darkest">Vue.js</span> is my favorite choice,
+                often together with two great CSS frameworks: The well-known
+                <span class="text-grey-darkest">Bootstrap</span> or the newly released
+                <span class="text-grey-darkest">TailwindCSS</span>
                 <br><br>
-                But I also used a bunch of other technologies, each depending on the projects aim and requirements!
+                Can't see parts of your tech-stack on that list? <span class="text-grey-darkest">Don't worry!</span>
+                Depending on the projects aim and requirements, I use way more technologies than listed above.
             </p>
         </div>
         <i class="fa fa-5x fa-caret-down mt-4 animated infinite bounceNice animation-duration-5 text-grey"></i>
