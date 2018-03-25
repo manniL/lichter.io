095b04b0e836c7ffdf7b1b675c1ca427bfcb1681
Alexander Lichter <manniL@gmx.net>
2018-03-26 00:05:57 +0200

    Update TailwindCss

--- tailwind.js
+++ tailwind.js
@@ -387,7 +387,7 @@ module.exports = {
   |
   */
 
-  borderColors: Object.assign({default: colors['grey-light']}, colors),
+  borderColors: Object.assign({ default: colors['grey-light'] }, colors),
 
   /*
   |-----------------------------------------------------------------------------
@@ -783,6 +783,24 @@ module.exports = {
   */
 
   modules: 'all',
+
+  /*
+  |-----------------------------------------------------------------------------
+  | Plugins                                https://tailwindcss.com/docs/plugins
+  |-----------------------------------------------------------------------------
+  |
+  | Here is where you can register any plugins you'd like to use in your
+  | project. Tailwind's built-in `container` plugin is enabled by default to
+  | give you a Bootstrap-style responsive container component out of the box.
+  |
+  | Be sure to view the complete plugin documentation to learn more about how
+  | the plugin system works.
+  |
+  */
+
+  plugins: [
+    require('tailwindcss/plugins/container')(),
+  ],
 
   /*
   |-----------------------------------------------------------------------------
