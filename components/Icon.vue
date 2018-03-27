4eda016244f15666db434b13290a28b55bf03a14
Alexander Lichter <manniL@gmx.net>
2018-03-27 18:16:55 +0200

    Extract Icon component and add tracking
    Resolves #2

---
+++ components/Icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    :viewBox="viewBox"
+    class="w-8 h-8 fill-current inline-block"
+    xmlns="http://www.w3.org/2000/svg">
+    <path :d="path"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    viewBox: {
+      type: String,
+      default: '0 -100 1792 1792'
+    },
+    path: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
