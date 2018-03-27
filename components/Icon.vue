797fb795a0f8f6fcab21086c3f87969d70e1a5f3
Alexander Lichter <manniL@gmx.net>
2018-03-27 18:35:54 +0200

    Use Icon in Navbar and Footer as well

--- components/Icon.vue
+++ components/Icon.vue
@@ -1,22 +1,33 @@
 <template>
   <svg
     :viewBox="viewBox"
-    class="w-8 h-8 fill-current inline-block"
+    :class="iconClass"
     xmlns="http://www.w3.org/2000/svg">
     <path :d="path"/>
+    <title
+      v-if="iconTitle"
+      v-text="iconTitle"/>
   </svg>
 </template>
 
 <script>
 export default {
   props: {
+    path: {
+      type: String,
+      required: true
+    },
     viewBox: {
       type: String,
       default: '0 -100 1792 1792'
     },
-    path: {
+    iconClass: {
       type: String,
-      required: true
+      default: 'w-8 h-8 fill-current inline-block'
+    },
+    iconTitle: {
+      type: String,
+      default: ''
     }
   }
 }
