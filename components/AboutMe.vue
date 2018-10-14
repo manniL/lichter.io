1cbcfcc03a07008495ab65720f6119b8324ea9b7
Alexander Lichter <manniL@gmx.net>
2018-10-14 13:21:15 +0100

    fix(about-me): increase padding-bottom a little
    resolves #20

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex flex-col flex-wrap items-center bg-grey-light pt-8 animated fade-in">
+  <section class="flex flex-col flex-wrap items-center bg-grey-light pt-8 pb-6 animated fade-in">
     <div class="justify-around items-center w-5/6 md:w-1/2 mx-auto">
       <p class="text-xl md:text-2xl text-black mb-2">
         Hey you!
