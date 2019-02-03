0d73d276f9e1cedf90337d007d6307e33006e038
Alexander Lichter <manniL@gmx.net>
2019-02-03 21:51:21 +0000

    refactor: use "modern" way of denoting ssr directives

--- config/render.js
+++ config/render.js
@@ -1,8 +1,7 @@
 export default {
   bundleRenderer: {
-    shouldPreload: (file, type) => ['script', 'style', 'font'].includes(type),
     directives: {
-      'v-scroll-reveal': (node) => {
+      scrollReveal(node) {
         const cssClass = node.data.class || (node.data.class = {})
         if (Array.isArray(cssClass)) {
           cssClass.push('opacity-0')
