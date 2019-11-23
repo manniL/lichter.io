ef1fbd788e1b8efd30a2890e826ccf0b58075415
Alexander Lichter <manniL@gmx.net>
2019-11-23 20:47:11 +0100

    refactor: use composition api for functional components
    removing "functional" attr to make it work. Functional components won't
    exist in Vue 3 anyway and don't give a meaningful perf boost in
    the current scenario

--- components/skills/Rating.vue
+++ components/skills/Rating.vue
@@ -1,8 +1,8 @@
 <script>
+import { createElement as h } from '@vue/composition-api'
 import RatingSquare from '~/components/skills/RatingSquare.vue'
 
 export default {
-  functional: true,
   components: { RatingSquare },
   props: {
     rating: {
@@ -11,12 +11,12 @@ export default {
       validator: value => value >= 0 && value <= 5
     }
   },
-  render (h, { props }) {
+  setup (props) {
     const squares = Array
       .from({ length: 5 }, (_, i) => i + 1)
       .map(i => h(RatingSquare, { props: { hidden: i > props.rating } }))
 
-    return h('div', { class: 'flex group' }, squares)
+    return () => h('div', { class: 'flex group' }, squares)
   }
 }
 </script>
--- components/skills/RatingSquare.vue
+++ components/skills/RatingSquare.vue
@@ -1,11 +1,14 @@
 <script>
+import { createElement as h } from '@vue/composition-api'
+
 export default {
-  functional: true,
-  render (h, { props }) {
+  setup (props) {
     const outerClasses = 'w-8 h-8 border-4 border-red-900 shadow-lg mx-1 bg-transparent group-hover:rotate-45 transition-all-300'
     const innerClasses = ['absolute w-4 h-4 m-1 bg-red-800 shadow-lg', { 'hidden': props.hidden }]
+
     const innerDiv = h('div', { class: innerClasses }, '')
-    return h('div', { class: outerClasses }, [innerDiv])
+
+    return () => h('div', { class: outerClasses }, [innerDiv])
   }
 }
 </script>
