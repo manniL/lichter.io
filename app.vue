8d7573844df546a71302727da2489c3cfff718c9
Alexander Lichter <github@lichter.io>
2022-12-05 15:39:01 +0100

    chore: cleanup

--- app.vue
+++ app.vue
@@ -8,8 +8,6 @@
 </template>
 
 <script setup lang="ts">
-// import Consent from 'vue-cookieconsent-component/src/components/CookieConsent.vue'
-// import IfBot from 'vue-if-bot/dist/vue-if-bot.es'
 const { siteUrl } = useRuntimeConfig()
 const route = useRoute()
 const canonical = computed(() => {
