45f40f21bab6138d81dc1ff781da21f19addc68f
Alexander Lichter <github@lichter.io>
2022-12-05 16:40:50 +0100

    refactor: cookie consent

--- components/CookieConsent.client.vue
+++ components/CookieConsent.client.vue
@@ -5,19 +5,11 @@ const acknowledgeCookie = useCookie('got_it', {
 const removeBanner = () => {
   acknowledgeCookie.value = 'true'
 }
+watch(acknowledgeCookie, (curr) => {
+  console.log(curr)
+})
 </script>
+
 <template>
-  <div role="dialog" v-show="!acknowledgeCookie"
-    class="w-full py-2 z-10 flex items-center justify-center bg-red-900 text-red-200 text-xs sticky md:text-sm md:text-center">
-    <div class="mx-2 my-1 px-2 md:px-4">
-      I use 🍪 for user analysis and on-page improvements!
-      More in the <NuxtLink class="text-red-100 underline hover:no-underline inline-block" to="/privacy/" no-prefetch>
-        Privacy Policy
-      </NuxtLink>
-    </div>
-    <button class="text-white py-2 rounded border-white border hover:bg-white hover:text-black mx-2 my-1 px-2 md:px-4"
-      @click="removeBanner">
-      Got it!
-    </button>
-  </div>
+  <CookieConsentContent v-if="!acknowledgeCookie" @close="removeBanner" />
 </template>
