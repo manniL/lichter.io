82f4a3f67010fb6d62faa3db1521f4e9daa0f1b4
Alexander <github@lichter.io>
2024-01-07 14:45:41 +0100

    refactor: code organization

--- app.vue
+++ app.vue
@@ -1,126 +1,135 @@
 <script setup lang="ts">
-const siteUrl = useSiteConfig().url
-const route = useRoute()
+useAppSeo()
+useSiteNotifications()
 
-const canonical = computed(() => {
-  const pathWithSlash = route.path.endsWith('/') ? route.path : `${route.path}/`
-  return `${siteUrl}${pathWithSlash}`
-})
+function useAppSeo() {
+  const siteUrl = useSiteConfig().url
+  const route = useRoute()
 
-const isDark = useDark()
-const favicon = computed(() => `/img/logo/glyph-${isDark.value ? 'white' : 'black'}-colored.svg`)
+  const canonical = computed(() => {
+    const pathWithSlash = route.path.endsWith('/') ? route.path : `${route.path}/`
+    return `${siteUrl}${pathWithSlash}`
+  })
 
-useHead({
-  // TODO: Do that in nuxt.config.ts
-  // TODO: Change this for German content
-  htmlAttrs: {
-    lang: 'en'
-  },
-  titleTemplate: (c) => c ? `${c} - Alexander Lichter` : 'Alexander Lichter - Web Engineering Consultant',
-  link: [
-    { rel: 'canonical', href: canonical },
-    { rel: 'icon', href: favicon, type: "image/svg+xml" },
-  ],
-  bodyAttrs: {
-    class: 'bg-black antialiased min-h-screen text-white'
-  },
-})
+  const isDark = useDark()
+  const favicon = computed(() => `/img/logo/glyph-${isDark.value ? 'white' : 'black'}-colored.svg`)
 
-useSchemaOrg([
-  definePerson({
-    address: {
-      '@type': 'PostalAddress',
-      addressCountry: 'NL',
-      addressLocality: 'Amsterdam',
-      addressRegion: 'Noord Holland',
-      postalCode: '1087 JK',
-      streetAddress: 'IJburglaan 1024'
+  useServerHead({
+    htmlAttrs: {
+      // TODO: Change this for German content
+      lang: 'en'
     },
-    name: 'Alexander Lichter',
-    image: '/img/me@2x.jpg',
-    email: 'mailto:hello@lichter.io',
-    nationality: "German",
-    jobTitle: 'Web Engineering Consultant & Managing Director',
-    url: 'https://www.lichter.io/',
-    sameAs: [
-      ...Object.values(SOCIALS),
-      'https://stackoverflow.com/users/3975480/mannil',
-      'https://www.developmint.de/',
-    ]
-  }),
-  defineWebSite(),
-  defineWebPage()
-])
+    bodyAttrs: {
+      class: 'bg-black antialiased min-h-screen text-white'
+    },
+  })
 
-useSeoMeta({
-  author: 'Alexander Lichter',
-  ogSiteName: 'www.lichter.io',
-  ogType: 'website'
-})
+  useHead({
+    titleTemplate: (c) => c ? `${c} - Alexander Lichter` : 'Alexander Lichter - Web Engineering Consultant',
+    link: [
+      { rel: 'canonical', href: canonical },
+      { rel: 'icon', href: favicon, type: "image/svg+xml" },
+    ],
+  })
 
-const styleConfig = {
-  hideEventTypeDetails: false,
-  layout: "month_view"
-}
+  useSchemaOrg([
+    definePerson({
+      address: {
+        '@type': 'PostalAddress',
+        addressCountry: 'NL',
+        addressLocality: 'Amsterdam',
+        addressRegion: 'Noord Holland',
+        postalCode: '1087 JK',
+        streetAddress: 'IJburglaan 1024'
+      },
+      name: 'Alexander Lichter',
+      image: '/img/me@2x.jpg',
+      email: 'mailto:hello@lichter.io',
+      nationality: "German",
+      jobTitle: 'Web Engineering Consultant & Managing Director',
+      url: 'https://www.lichter.io/',
+      sameAs: [
+        ...Object.values(SOCIALS),
+        'https://stackoverflow.com/users/3975480/mannil',
+        'https://www.developmint.de/',
+      ]
+    }),
+    defineWebSite(),
+    defineWebPage()
+  ])
 
-// Cal.com
-// TODO: Defer loading later on but store clicks in the meantime
-useHead({
-  script: [
-    {
-      key: 'cal',
-      innerHTML: `
+  useServerSeoMeta({
+    author: 'Alexander Lichter',
+    ogSiteName: 'www.lichter.io',
+    ogType: 'website'
+  })
+
+  const styleConfig = {
+    hideEventTypeDetails: false,
+    layout: "month_view"
+  }
+
+  // Cal.com
+  // TODO: Defer loading later on but store clicks in the meantime
+  useHead({
+    script: [
+      {
+        key: 'cal',
+        innerHTML: `
 (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; typeof namespace === "string" ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar); return; } p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
 Cal("init", {origin:"https://cal.com"});
 Cal("ui", ${JSON.stringify(styleConfig)});
 `,
-    }
-  ]
-})
-
-const { addNotification } = useNotifications()
-
-onMounted(async () => {
-  const result = onDicsordRef()
-  if (result) {
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-  }
-  onUserSpeaksGerman()
-})
-
-function onDicsordRef(): Boolean {
-  const isDicsordDomain = route.query?.ref === 'dicsord.com'
-  if (!isDicsordDomain) {
-    return false
-  }
-
-  addNotification({
-    heading: 'You should double check the URL ;)',
-    body: 'Did you want to go to https://discord.com/ instead?'
+      }
+    ]
   })
-  return true
 }
 
-function onUserSpeaksGerman(): Boolean {
-  const doesSpeakGerman = navigator.languages.some(lang => lang.startsWith('de'))
-  if (!doesSpeakGerman) {
-    return false
-  }
+function useSiteNotifications() {
+  const { addNotification } = useNotifications()
 
-  const didUserSeeGermanNotificationAlready = localStorage.getItem(LOCALSTORAGE_KEYS.notificationGerman)
-  if (didUserSeeGermanNotificationAlready) {
-    return false
-  }
-
-  addNotification({
-    heading: 'Ich spreche auch Deutsch!',
-    body: 'Alle Workshops und Talks können auch auf Deutsch gehalten werden.',
-    onRemove() {
-      localStorage.setItem(LOCALSTORAGE_KEYS.notificationGerman, 'true')
+  onMounted(async () => {
+    const result = onDicsordRef()
+    if (result) {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
     }
+    onUserSpeaksGerman()
   })
 
-  return true
+  function onDicsordRef(): Boolean {
+    const isDicsordDomain = route.query?.ref === 'dicsord.com'
+    if (!isDicsordDomain) {
+      return false
+    }
+
+    addNotification({
+      heading: 'You should double check the URL ;)',
+      body: 'Did you want to go to https://discord.com/ instead?'
+    })
+    return true
+  }
+
+  function onUserSpeaksGerman(): Boolean {
+    const doesSpeakGerman = navigator.languages.some(lang => lang.startsWith('de'))
+    if (!doesSpeakGerman) {
+      return false
+    }
+
+    const didUserSeeGermanNotificationAlready = localStorage.getItem(LOCALSTORAGE_KEYS.notificationGerman)
+    if (didUserSeeGermanNotificationAlready) {
+      return false
+    }
+
+    addNotification({
+      heading: 'Ich spreche auch Deutsch!',
+      body: 'Alle Workshops und Talks können auch auf Deutsch gehalten werden.',
+      onRemove() {
+        localStorage.setItem(LOCALSTORAGE_KEYS.notificationGerman, 'true')
+      }
+    })
+
+    return true
+  }
 }
 </script>
 
