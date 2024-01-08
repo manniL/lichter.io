e884d292e5b5a0ac23661b9df59aa2f8461a9f45
Harlan Wilton <harlan@harlanzw.com>
2024-01-08 16:07:41 +1100

    chore: revert type

--- pages/workshops/index.vue
+++ pages/workshops/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { type QueryBuilderParams } from '@nuxt/content/dist/runtime/types';
+import { type QueryBuilderParams } from '@nuxt/content/dist/runtime/types/index.js';
 
 definePageMeta({
   documentDriven: false
