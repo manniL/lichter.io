850dcc4b9666d94d7ee1b791493cf91bea2a4a40
Alexander Lichter <github@lichter.io>
2024-12-16 14:32:45 +0100

    fix: gradient icons

--- components/Service/Preview.vue
+++ components/Service/Preview.vue
@@ -9,7 +9,7 @@ defineProps<{
 <template>
   <AppLink :to="to" class="flex-1 flex flex-col items-center group">
     <div class="bg-clip-text bg-gradient-to-r from-red-600 to-pink-700">
-      <Icon :name="icon" class="transition-all group-hover:scale-125 text-6xl icon-gradient" />
+      <Icon mode="svg" :name="icon" class="transition-all group-hover:scale-125 text-6xl icon-gradient" />
     </div>
     <AppParagraph tag="h2" class="mt-4 text-2xl font-semibold">
       {{ title }}
